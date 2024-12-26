7696d114eca6fcaf0c60df778c7f772fec29224d
JovTim <202280218@psu.palawan.edu.ph>
2024-12-26 15:19:15 +0800

    added user control form for adding student folder

---
+++ folder_management/addStudentFolder.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace folder_management
+{
+    partial class addStudentFolder
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            addCancelButton = new Button();
+            profileUrlTextBox = new TextBox();
+            label2 = new Label();
+            firstNameTextBox = new TextBox();
+            lastNameTextBox = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            middleNameTextBox = new TextBox();
+            label5 = new Label();
+            yearTextBox = new TextBox();
+            label6 = new Label();
+            blockTextBox = new TextBox();
+            label7 = new Label();
+            codeTextBox = new TextBox();
+            label8 = new Label();
+            contentListsComboBox = new ComboBox();
+            label9 = new Label();
+            addNewContent = new TextBox();
+            contentLists = new ListView();
+            listContents = new ColumnHeader();
+            panel1 = new Panel();
+            photoPreviewBox = new PictureBox();
+            label10 = new Label();
+            studentNumberTextBox = new TextBox();
+            label11 = new Label();
+            insertButton = new Button();
+            addContentButton = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)photoPreviewBox).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(4, 9);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(461, 54);
+            label1.TabIndex = 13;
+            label1.Text = "ADD STUDENT FOLDER";
+            // 
+            // addCancelButton
+            // 
+            addCancelButton.BackColor = SystemColors.AppWorkspace;
+            addCancelButton.FlatAppearance.BorderColor = SystemColors.ControlLight;
+            addCancelButton.FlatAppearance.BorderSize = 0;
+            addCancelButton.FlatStyle = FlatStyle.Flat;
+            addCancelButton.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            addCancelButton.Location = new Point(958, 635);
+            addCancelButton.Name = "addCancelButton";
+            addCancelButton.Size = new Size(129, 59);
+            addCancelButton.TabIndex = 14;
+            addCancelButton.Text = "CANCEL";
+            addCancelButton.UseVisualStyleBackColor = false;
+            addCancelButton.Click += addCancelButton_Click;
+            // 
+            // profileUrlTextBox
+            // 
+            profileUrlTextBox.BorderStyle = BorderStyle.FixedSingle;
+            profileUrlTextBox.Location = new Point(20, 119);
+            profileUrlTextBox.Name = "profileUrlTextBox";
+            profileUrlTextBox.Size = new Size(526, 34);
+            profileUrlTextBox.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(20, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(292, 28);
+            label2.TabIndex = 16;
+            label2.Text = "GOOGLE DRIVE PICTURE LINK";
+            // 
+            // firstNameTextBox
+            // 
+            firstNameTextBox.BorderStyle = BorderStyle.FixedSingle;
+            firstNameTextBox.Location = new Point(20, 267);
+            firstNameTextBox.Name = "firstNameTextBox";
+            firstNameTextBox.Size = new Size(251, 34);
+            firstNameTextBox.TabIndex = 17;
+            // 
+            // lastNameTextBox
+            // 
+            lastNameTextBox.BorderStyle = BorderStyle.FixedSingle;
+            lastNameTextBox.Location = new Point(295, 267);
+            lastNameTextBox.Name = "lastNameTextBox";
+            lastNameTextBox.Size = new Size(251, 34);
+            lastNameTextBox.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(20, 236);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 28);
+            label3.TabIndex = 19;
+            label3.Text = "FIRST NAME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(295, 236);
+            label4.Name = "label4";
+            label4.Size = new Size(125, 28);
+            label4.TabIndex = 20;
+            label4.Text = "LAST NAME";
+            // 
+            // middleNameTextBox
+            // 
+            middleNameTextBox.BorderStyle = BorderStyle.FixedSingle;
+            middleNameTextBox.Location = new Point(20, 347);
+            middleNameTextBox.Name = "middleNameTextBox";
+            middleNameTextBox.Size = new Size(251, 34);
+            middleNameTextBox.TabIndex = 21;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(20, 316);
+            label5.Name = "label5";
+            label5.Size = new Size(154, 28);
+            label5.TabIndex = 22;
+            label5.Text = "MIDDLE NAME";
+            // 
+            // yearTextBox
+            // 
+            yearTextBox.BorderStyle = BorderStyle.FixedSingle;
+            yearTextBox.Location = new Point(295, 347);
+            yearTextBox.Name = "yearTextBox";
+            yearTextBox.Size = new Size(70, 34);
+            yearTextBox.TabIndex = 23;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(295, 316);
+            label6.Name = "label6";
+            label6.Size = new Size(62, 28);
+            label6.TabIndex = 24;
+            label6.Text = "YEAR";
+            // 
+            // blockTextBox
+            // 
+            blockTextBox.BorderStyle = BorderStyle.FixedSingle;
+            blockTextBox.Location = new Point(384, 347);
+            blockTextBox.Name = "blockTextBox";
+            blockTextBox.Size = new Size(70, 34);
+            blockTextBox.TabIndex = 25;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(384, 316);
+            label7.Name = "label7";
+            label7.Size = new Size(75, 28);
+            label7.TabIndex = 26;
+            label7.Text = "BLOCK";
+            // 
+            // codeTextBox
+            // 
+            codeTextBox.BorderStyle = BorderStyle.FixedSingle;
+            codeTextBox.Location = new Point(476, 347);
+            codeTextBox.Name = "codeTextBox";
+            codeTextBox.Size = new Size(70, 34);
+            codeTextBox.TabIndex = 27;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(476, 316);
+            label8.Name = "label8";
+            label8.Size = new Size(65, 28);
+            label8.TabIndex = 28;
+            label8.Text = "CODE";
+            // 
+            // contentListsComboBox
+            // 
+            contentListsComboBox.FormattingEnabled = true;
+            contentListsComboBox.Location = new Point(20, 421);
+            contentListsComboBox.Name = "contentListsComboBox";
+            contentListsComboBox.Size = new Size(292, 36);
+            contentListsComboBox.TabIndex = 29;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(20, 390);
+            label9.Name = "label9";
+            label9.Size = new Size(197, 28);
+            label9.TabIndex = 30;
+            label9.Text = "FOLDER CONTENTS";
+            // 
+            // addNewContent
+            // 
+            addNewContent.BorderStyle = BorderStyle.FixedSingle;
+            addNewContent.Location = new Point(20, 474);
+            addNewContent.Name = "addNewContent";
+            addNewContent.Size = new Size(292, 34);
+            addNewContent.TabIndex = 31;
+            // 
+            // contentLists
+            // 
+            contentLists.Columns.AddRange(new ColumnHeader[] { listContents });
+            contentLists.Location = new Point(20, 526);
+            contentLists.Name = "contentLists";
+            contentLists.Size = new Size(526, 176);
+            contentLists.TabIndex = 32;
+            contentLists.UseCompatibleStateImageBehavior = false;
+            contentLists.View = View.Details;
+            // 
+            // listContents
+            // 
+            listContents.Text = "FOLDER CONTENTS";
+            listContents.Width = 520;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(photoPreviewBox);
+            panel1.Controls.Add(label10);
+            panel1.Location = new Point(644, 88);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(443, 364);
+            panel1.TabIndex = 33;
+            // 
+            // photoPreviewBox
+            // 
+            photoPreviewBox.Location = new Point(3, 31);
+            photoPreviewBox.Name = "photoPreviewBox";
+            photoPreviewBox.Size = new Size(437, 330);
+            photoPreviewBox.TabIndex = 35;
+            photoPreviewBox.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(3, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(171, 28);
+            label10.TabIndex = 34;
+            label10.Text = "PHOTO PREVIEW";
+            // 
+            // studentNumberTextBox
+            // 
+            studentNumberTextBox.BorderStyle = BorderStyle.FixedSingle;
+            studentNumberTextBox.Location = new Point(20, 191);
+            studentNumberTextBox.Name = "studentNumberTextBox";
+            studentNumberTextBox.Size = new Size(251, 34);
+            studentNumberTextBox.TabIndex = 34;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(20, 160);
+            label11.Name = "label11";
+            label11.Size = new Size(195, 28);
+            label11.TabIndex = 35;
+            label11.Text = "STUDENT NUMBER";
+            // 
+            // insertButton
+            // 
+            insertButton.BackColor = Color.FromArgb(92, 225, 230);
+            insertButton.FlatAppearance.BorderColor = Color.FromArgb(92, 225, 230);
+            insertButton.FlatAppearance.BorderSize = 0;
+            insertButton.FlatStyle = FlatStyle.Flat;
+            insertButton.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            insertButton.Location = new Point(823, 635);
+            insertButton.Name = "insertButton";
+            insertButton.Size = new Size(129, 59);
+            insertButton.TabIndex = 36;
+            insertButton.Text = "INSERT";
+            insertButton.UseVisualStyleBackColor = false;
+            // 
+            // addContentButton
+            // 
+            addContentButton.BackColor = Color.FromArgb(92, 225, 230);
+            addContentButton.FlatAppearance.BorderColor = Color.FromArgb(92, 225, 230);
+            addContentButton.FlatAppearance.BorderSize = 0;
+            addContentButton.FlatStyle = FlatStyle.Flat;
+            addContentButton.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            addContentButton.Location = new Point(350, 469);
+            addContentButton.Name = "addContentButton";
+            addContentButton.Size = new Size(104, 39);
+            addContentButton.TabIndex = 37;
+            addContentButton.Text = "ADD";
+            addContentButton.UseVisualStyleBackColor = false;
+            // 
+            // addStudentFolder
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            Controls.Add(addContentButton);
+            Controls.Add(insertButton);
+            Controls.Add(label11);
+            Controls.Add(studentNumberTextBox);
+            Controls.Add(panel1);
+            Controls.Add(contentLists);
+            Controls.Add(addNewContent);
+            Controls.Add(label9);
+            Controls.Add(contentListsComboBox);
+            Controls.Add(label8);
+            Controls.Add(codeTextBox);
+            Controls.Add(label7);
+            Controls.Add(blockTextBox);
+            Controls.Add(label6);
+            Controls.Add(yearTextBox);
+            Controls.Add(label5);
+            Controls.Add(middleNameTextBox);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(lastNameTextBox);
+            Controls.Add(firstNameTextBox);
+            Controls.Add(label2);
+            Controls.Add(profileUrlTextBox);
+            Controls.Add(addCancelButton);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "addStudentFolder";
+            Size = new Size(1104, 708);
+            Load += addStudentFolder_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)photoPreviewBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Button addCancelButton;
+        private TextBox profileUrlTextBox;
+        private Label label2;
+        private TextBox firstNameTextBox;
+        private TextBox lastNameTextBox;
+        private Label label3;
+        private Label label4;
+        private TextBox middleNameTextBox;
+        private Label label5;
+        private TextBox yearTextBox;
+        private Label label6;
+        private TextBox blockTextBox;
+        private Label label7;
+        private TextBox codeTextBox;
+        private Label label8;
+        private ComboBox contentListsComboBox;
+        private Label label9;
+        private TextBox addNewContent;
+        private ListView contentLists;
+        private ColumnHeader listContents;
+        private Panel panel1;
+        private PictureBox photoPreviewBox;
+        private Label label10;
+        private TextBox studentNumberTextBox;
+        private Label label11;
+        private Button insertButton;
+        private Button addContentButton;
+    }
+}
---
+++ folder_management/addStudentFolder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folder_management
+{
+    public partial class addStudentFolder : UserControl
+    {
+        public event Action SwitchToPreviousControl;
+        public addStudentFolder()
+        {
+            InitializeComponent();
+        }
+
+        private void addStudentFolder_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addCancelButton_Click(object sender, EventArgs e)
+        {
+            SwitchToPreviousControl?.Invoke();
+        }
+
+
+        /*
+        private void addCloseButton_Click(object sender, EventArgs e)
+        {
+            SwitchToPreviousControl?.Invoke();
+        }
+
+        */
+    }
+}
