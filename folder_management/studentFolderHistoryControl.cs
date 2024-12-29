4cf2aae177a15a070f5fa8d03cfc3f01d58a28ba
JovTim <202280218@psu.palawan.edu.ph>
2024-12-29 20:45:41 +0800

    adding draft function to archive button

--- folder_management/studentFolderHistoryControl.cs
+++ folder_management/studentFolderHistoryControl.cs
@@ -26,26 +26,6 @@ namespace folder_management
             historyFolderLoad();
         }
 
-        private void sampleData()
-        {
-            List<string[]> sampleData = new List<string[]>
-                {
-                    new string[] {"202287413", "FORD, HENRY CIVIL", "RETURNED", "2022-12-12", "A01", "" },
-                    new string[] {"202280121", "GANA, SARA MAL", "BORROWED", "2022-12-12", "A21", "" },
-                    new string[] { "202370131", "WATSON, GARY SMILE", "ARCHIVED", "2022-12-12", "B12", "" },
-                    new string[] { "202150431", "MONROE, JAMES PENNY", "RETURNED", "2022-12-12", "C15", "" },
-                    new string[] { "202290001", "ADAMS, LOUISE -", "UPDATED", "2022-12-12", "A19", "YEAR, BLOCK" },
-                    new string[] { "202103123", "MANSON, HARRY STYLES", "ADDED", "2022-12-12", "B41", "" },
-                    new string[] { "202160132", "WILLIAM, BOB MARLEY", "BORROWED", "2022-12-12", "A09", "" },
-                    new string[] { "202301231", "PARKER, JANE -", "MISSING", "2022-12-12", "A05", "" },
-                };
-
-            foreach (var row in sampleData)
-            {
-                historyList.Rows.Add(row);
-            }
-        }
-
         private void historyFolderLoad()
         {
             if (dataAccess == null)
