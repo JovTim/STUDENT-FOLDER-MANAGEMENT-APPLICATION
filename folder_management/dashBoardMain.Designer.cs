6be373156d3c56882b29973d46ff0844f0a2c682
JovTim <202280218@psu.palawan.edu.ph>
2025-01-03 02:05:14 +0800

    added dashboard draft design

--- folder_management/dashBoardMain.Designer.cs
+++ folder_management/dashBoardMain.Designer.cs
@@ -28,35 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            digitalClock1 = new digitalClock();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
             SuspendLayout();
             // 
-            // label1
+            // digitalClock1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(521, 354);
-            label1.Name = "label1";
-            label1.Size = new Size(511, 106);
-            label1.TabIndex = 0;
-            label1.Text = "DASHBOARD";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
+            digitalClock1.Location = new Point(27, 0);
+            digitalClock1.Name = "digitalClock1";
+            digitalClock1.Size = new Size(348, 142);
+            digitalClock1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(92, 225, 230);
+            panel1.Location = new Point(585, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(811, 176);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(233, 233, 233);
+            panel2.Location = new Point(25, 211);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(309, 124);
+            panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(166, 166, 166);
+            panel3.Location = new Point(380, 211);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(309, 124);
+            panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(245, 138, 138);
+            panel4.Location = new Point(737, 211);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(309, 124);
+            panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(255, 222, 89);
+            panel5.Location = new Point(1087, 211);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(309, 124);
+            panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(92, 225, 230);
+            panel6.Location = new Point(25, 360);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(423, 176);
+            panel6.TabIndex = 3;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.FromArgb(92, 225, 230);
+            panel7.Location = new Point(498, 360);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(423, 176);
+            panel7.TabIndex = 4;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.FromArgb(92, 225, 230);
+            panel8.Location = new Point(973, 360);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(423, 176);
+            panel8.TabIndex = 4;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = Color.FromArgb(92, 225, 230);
+            panel9.Location = new Point(25, 561);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(423, 176);
+            panel9.TabIndex = 4;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = Color.FromArgb(92, 225, 230);
+            panel10.Location = new Point(498, 561);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(423, 176);
+            panel10.TabIndex = 4;
             // 
             // dashBoardMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            Controls.Add(label1);
+            Controls.Add(panel10);
+            Controls.Add(panel9);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(digitalClock1);
             Name = "dashBoardMain";
             Size = new Size(1473, 740);
             Load += dashBoardMain_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
+        private digitalClock digitalClock1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel10;
     }
 }
--- folder_management/dashBoardMain.cs
+++ folder_management/dashBoardMain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,18 @@ namespace folder_management
 {
     public partial class dashBoardMain : UserControl
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
         sqliteDataAccess dataAccess;
         private static bool isCheckFoldersCalled;
         public dashBoardMain()
@@ -25,15 +38,48 @@ namespace folder_management
             if (!isCheckFoldersCalled) // Check if it has been called
             {
                 checkFolders();
-                isCheckFoldersCalled = true; // Set the flag to true
-                //MessageBox.Show("I CHECKED THE FOLDER STATUS!", "Title");
+                isCheckFoldersCalled = true;
             }
+            panelRoundEdges();
         }
 
 
         public void checkFolders()
         {
             dataAccess.checkEncodeDates();
+        }
+
+        public void panelRoundEdges()
+        {
+            panel1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel1.Width,
+            panel1.Height, 30, 30));
+
+            panel2.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel2.Width,
+            panel2.Height, 100, 100));
+
+            panel3.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel3.Width,
+            panel3.Height, 100, 100));
+
+            panel4.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel4.Width,
+            panel4.Height, 100, 100));
+
+            panel5.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel5.Width,
+            panel5.Height, 100, 100));
+
+            panel6.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel6.Width,
+            panel6.Height, 30, 30));
+
+            panel7.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel7.Width,
+            panel7.Height, 30, 30));
+
+            panel8.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel8.Width,
+            panel8.Height, 30, 30));
+
+            panel9.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel9.Width,
+            panel9.Height, 30, 30));
+
+            panel10.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel10.Width,
+            panel10.Height, 30, 30));
         }
     }
 }
