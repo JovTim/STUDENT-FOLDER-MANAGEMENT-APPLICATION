9e7cc95ef68d4cbf65a7500b05cc96a6c3342ff0
JovTim <202280218@psu.palawan.edu.ph>
2025-01-04 23:49:25 +0800

    added validation

--- folder_management/addStudentFolder.cs
+++ folder_management/addStudentFolder.cs
@@ -15,12 +15,14 @@ namespace folder_management
         public event Action? SwitchToPreviousControl;
         private sqliteDataAccess dataAccess;
         private programTools? programTools;
+        private verificationTool? verificationTool;
         public addStudentFolder()
         {
             InitializeComponent();
 
             dataAccess = new sqliteDataAccess();
             programTools = new programTools();
+            verificationTool = new verificationTool();
         }
 
         private void addStudentFolder_Load(object sender, EventArgs e)
@@ -35,19 +37,64 @@ namespace folder_management
 
         private void insertButton_Click(object sender, EventArgs e)
         {
-            string profileUrl = programTools.convertToDownloadableLink(profileUrlTextBox.Text);
+            if (ValidateInputs() && !(ValidateInputs2()))
+            {
+                string profileUrl = string.IsNullOrWhiteSpace(profileUrlTextBox.Text)
+                    ? "https://drive.google.com/uc?export=download&id=1mVxkZkivUqkKST3CePQ72O93N135VHxw"
+                    : programTools.convertToDownloadableLink(profileUrlTextBox.Text);
 
-            dataAccess.insertFolder(profileUrl, studentNumberTextBox.Text, firstNameTextBox.Text, lastNameTextBox.Text, middleNameTextBox.Text, Convert.ToInt32(yearTextBox.Text), Convert.ToInt32(blockTextBox.Text), 1, codeTextBox.Text);
+                string middleName = string.IsNullOrWhiteSpace(middleNameTextBox.Text) ? "-" : middleNameTextBox.Text;
 
-            profileUrlTextBox.Text = String.Empty;
-            studentNumberTextBox.Text = String.Empty;
-            firstNameTextBox.Text = String.Empty;
-            lastNameTextBox.Text = String.Empty;
-            middleNameTextBox.Text = String.Empty;
-            yearTextBox.Text = String.Empty;
-            blockTextBox.Text = String.Empty;
-            codeTextBox.Text = String.Empty;
+                dataAccess.insertFolder(
+                    profileUrl,
+                    studentNumberTextBox.Text,
+                    firstNameTextBox.Text,
+                    lastNameTextBox.Text,
+                    middleName,
+                    Convert.ToInt32(yearTextBox.Text),
+                    Convert.ToInt32(blockTextBox.Text),
+                    1,
+                    codeTextBox.Text);
 
+                ClearTextBoxes();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input/s. Please try again!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private bool ValidateInputs()
+        {
+            return verificationTool.validateUrl(profileUrlTextBox.Text) &&
+                   verificationTool.validateName(firstNameTextBox.Text) &&
+                   verificationTool.validateName(lastNameTextBox.Text) &&
+                   verificationTool.validateName(middleNameTextBox.Text) &&
+                   verificationTool.validateNumber(yearTextBox.Text) &&
+                   verificationTool.validateNumber(blockTextBox.Text) &&
+                   verificationTool.validateCode(codeTextBox.Text);
+        }
+
+        private bool ValidateInputs2()
+        {
+            return string.IsNullOrWhiteSpace(firstNameTextBox.Text) &&
+                    string.IsNullOrWhiteSpace(lastNameTextBox.Text) &&
+                    string.IsNullOrWhiteSpace(yearTextBox.Text) &&
+                    string.IsNullOrWhiteSpace(blockTextBox.Text) &&
+                    string.IsNullOrWhiteSpace(codeTextBox.Text);
+        }
+
+        private void ClearTextBoxes()
+        {
+            profileUrlTextBox.Text = string.Empty;
+            studentNumberTextBox.Text = string.Empty;
+            firstNameTextBox.Text = string.Empty;
+            lastNameTextBox.Text = string.Empty;
+            middleNameTextBox.Text = string.Empty;
+            yearTextBox.Text = string.Empty;
+            blockTextBox.Text = string.Empty;
+            codeTextBox.Text = string.Empty;
         }
     }
 }
--- folder_management/programTools.cs
+++ folder_management/programTools.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace folder_management
@@ -28,5 +29,60 @@ namespace folder_management
             }
             return false;
         }
+    }
+
+    public class verificationTool
+    {
+        public bool validateUrl(string url)
+        {
+            var urlRegex = new Regex(
+                @"^(https?|ftps?):\/\/(?:[a-zA-Z0-9]" +
+                @"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}" +
+                @"(?::(?:0|[1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}" +
+                @"|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?" +
+                @"(?:\/(?:[-a-zA-Z0-9@%_\+.~#?&=]+\/?)*)?$",
+                RegexOptions.IgnoreCase);
+
+            if (!urlRegex.IsMatch(url))
+                return false;
+
+            var googleDriveRegex = new Regex(
+                @"^(https?:\/\/)?(www\.)?(drive\.google\.com\/(file\/d\/|uc\?|open\?))",
+                RegexOptions.IgnoreCase);
+
+            return googleDriveRegex.IsMatch(url);
+        }
+
+        public bool validateName(string name)
+        {
+            var nameRegex = new Regex(@"^[a-zA-Z\s\-]+$");
+
+            return nameRegex.IsMatch(name);
+        }
+
+        public bool validateNumber(string input)
+        {
+            int number = 0;
+            if (!int.TryParse(input, out number)) return false;
+
+            int output = Convert.ToInt32(input);
+
+            if (output < 0) return false;
+
+            return true;
+            /*
+            var numberRegex = new Regex(@"^\d+$");
+
+            return numberRegex.IsMatch(input);
+            */
+        }
+
+        public bool validateCode(string input)
+        {
+            var combinedRegex = new Regex(@"^[a-zA-Z0-9\s\-]+$");
+
+            return combinedRegex.IsMatch(input);
+        }
+
     }
 }
